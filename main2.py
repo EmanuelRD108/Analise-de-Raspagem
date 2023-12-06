939ffb2e01c5a1548fd4202a5918e7e06222a1b5
Emanuel Rangel Dantas <108035826+EmanuelRD108@users.noreply.github.com>
2023-12-06 17:26:04 -0300

    Add Final

--- main2.py
+++ main2.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import plotly.express as px
 
 polaridade_por_autor = defaultdict(lambda: {'polaridade_acumulada': 0, 'num_frases': 0, 'frases': []})
+frases_polaridades = []
 
 for x in range(1, 11):
     url = f'https://quotes.toscrape.com/page/{x}'
@@ -26,25 +27,31 @@ for x in range(1, 11):
             polaridade_por_autor[autor]['num_frases'] += 1
             polaridade_por_autor[autor]['frases'].append((frase, polaridade))
 
+            frases_polaridades.append((frase, polaridade, autor))
+
     else:
         print("Falha, status: {}".format(response.status_code))
 
 autores_lista = list(polaridade_por_autor.keys())
 media_polaridade = [polaridade_por_autor[autor]['polaridade_acumulada'] / polaridade_por_autor[autor]['num_frases'] for autor in autores_lista]
 
-autor_mais_positivo = autores_lista[media_polaridade.index(max(media_polaridade))]
-autor_mais_negativo = autores_lista[media_polaridade.index(min(media_polaridade))]
+autor_mais_positivo = max(polaridade_por_autor, key=lambda autor: media_polaridade[autores_lista.index(autor)])
+autor_mais_negativo = min(polaridade_por_autor, key=lambda autor: media_polaridade[autores_lista.index(autor)])
 
-frase_mais_positiva = max(polaridade_por_autor[autor_mais_positivo]['frases'], key=lambda x: x[1])
-frase_mais_negativa = min(polaridade_por_autor[autor_mais_negativo]['frases'], key=lambda x: x[1])
+frase_mais_positiva = max(frases_polaridades, key=lambda x: x[1])
+frase_mais_negativa = min(frases_polaridades, key=lambda x: x[1])
 
 print('\nResultados:')
 print(f'\nAutor mais positivo: {autor_mais_positivo} | Polaridade: {max(media_polaridade):.4f}')
 print(f'Autor mais negativo: {autor_mais_negativo} | Polaridade: {min(media_polaridade):.4f}')
-print(f'Frase mais positiva {autor_mais_positivo}: {frase_mais_positiva[0]} | Polaridade: {frase_mais_positiva[1]:.4f}')
-print(f'Frase mais negativa {autor_mais_negativo}: {frase_mais_negativa[0]} | Polaridade: {frase_mais_negativa[1]:.4f}')
+print(f'\nFrase mais positiva ({frase_mais_positiva[2]}): {frase_mais_positiva[0]} | Polaridade: {frase_mais_positiva[1]:.4f}')
+print(f'Frase mais negativa ({frase_mais_negativa[2]}): {frase_mais_negativa[0]} | Polaridade: {frase_mais_negativa[1]:.4f}')
 
-fig = px.scatter(x=autores_lista, y=media_polaridade, labels={'x': 'Autores', 'y': 'Média da Positividade'})
-fig.update_layout(title='Média da Positividade das Frases por Autor', xaxis_title='Autores', yaxis_title='Média da Positividade')
+frases, polaridades, autores = zip(*frases_polaridades)
+fig = px.scatter(x=range(1, len(frases) + 1), y=polaridades, labels={'x': 'Frases', 'y': 'Polaridade'}, color=autores)
+fig.update_layout(title='Polaridade de Todas as Frases', xaxis_title='Frases', yaxis_title='Polaridade')
 fig.show()
+
+
+
 
